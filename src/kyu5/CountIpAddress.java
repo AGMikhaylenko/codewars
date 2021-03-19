766302f31085d5480664b9bba8c56a47c498b3cf
Alexander <agmikhaylenko@gmail.com>
2021-03-20 01:10:41 +0300

    add solution of Kata "CountIpAdress" (5kyu)

--- src/kyu5/CountIpAddress.java
+++ src/kyu5/CountIpAddress.java
@@ -8,22 +8,23 @@ package kyu5;
 public class CountIpAddress {
 
     public static void main(String[] args) {
-        System.out.println(ipsBetween("10.0.0.0", "10.0.1.0"));
+        System.out.println(ipsBetween("150.0.0.0", "150.0.0.1"));
     }
 
     public static long ipsBetween(String start, String end) {
+        System.out.println(start + end);
         String[] startAr = start.split("\\.");
         String[] endAr = end.split("\\.");
 
 
-        byte[] startByte = new byte[startAr.length];
-        byte[] endByte = new byte[endAr.length];
+        int[] startByte = new int[startAr.length];
+        int[] endByte = new int[endAr.length];
 
         long result = 0;
 
         for (int i = 0; i < 4; i++) {
-            startByte[i] = Byte.parseByte(startAr[i]);
-            endByte[i] = Byte.parseByte(endAr[i]);
+            startByte[i] = Integer.parseInt(startAr[i]);
+            endByte[i] = Integer.parseInt(endAr[i]);
 
             result += (endByte[i] - startByte[i]) * ((int) Math.pow(256, 3 - i));
         }
